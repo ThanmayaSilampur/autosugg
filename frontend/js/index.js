fdd7a6632955f33affc831c6890ff74137dc7708
Thanmaya Silampur <silampurthanmaya@gmail.com>
2026-06-20 10:04:04 +0530

    added toggle

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,14 +1,1 @@
-//alert("hello from js!");
-
-console.log("hello from js!");
-
-const gender = document.getElementById("gender");
-
-if (gender) {
-    gender.addEventListener("click", () => {
-        gender.textContent =
-            gender.textContent === "Male"
-                ? "Female"
-                : "Male";
-    });
-}
+console.log("hello world");
